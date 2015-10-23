a94e37737e8b0367d3034e4a690c8bf66f5d9a40
Christian Reyes A <creyes@utemvirtual.cl>
2015-10-23 16:37:44 -0300

    UPD:
    * Actualización del archivo gitignore que impide que se cargue el archivo "protected/runtime/application.log"

--- protected/runtime/gii-1.1.16/ModelCode.php
+++ protected/runtime/gii-1.1.16/ModelCode.php
@@ -5,6 +5,6 @@ return array (
   'tablePrefix' => '',
   'modelPath' => 'application.modules.repositorio.models',
   'baseClass' => 'CActiveRecord',
-  'buildRelations' => '1',
+  'buildRelations' => '0',
   'commentsAsLabels' => '0',
 );
